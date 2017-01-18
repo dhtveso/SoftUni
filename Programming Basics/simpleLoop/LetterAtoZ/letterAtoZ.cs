349534475ff9336751ee24ce6c8a4b95788a9a75
dhtveso <cska48sofia@gmail.com>
2017-01-18 10:41:52 +0200

    ADD folder simpleLoop

---
+++ Programming Basics/simpleLoop/LetterAtoZ/letterAtoZ.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace letterAtoZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var letter = 'a'; letter <= 'z'; letter++)
+            {
+                Console.WriteLine(letter);
+            }
+        }
+    }
+}
---
+++ Programming Basics/simpleLoop/sumNumbers/sumNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumNumbers
+{
+    class sumNumbers
+    {
+        static void Main(string[] args)
+        {
+            var Num = int.Parse(Console.ReadLine());
+            var sum = 0;
+            for (int i = 0; i < Num; i++)
+            {
+                var cifri = int.Parse(Console.ReadLine());
+                sum += cifri;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
